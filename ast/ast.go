56f2d1b7854db67258deea455af181ff38f04d68
rajrkane <raj.r.kane@protonmail.com>
2022-07-28 13:31:49 -0700

    parses function literals

--- ast/ast.go
+++ ast/ast.go
@@ -5,6 +5,7 @@ package ast
 import (
 	"bytes"
 	"intInGo/token"
+	"strings"
 )
 
 type Node interface {
@@ -176,6 +177,30 @@ func (il *IntegerLiteral) TokenLiteral() string {
 }
 func (il *IntegerLiteral) String() string {
 	return il.Token.Literal
+}
+
+type FunctionLiteral struct {
+	Token      token.Token // fn token
+	Parameters []*Identifier
+	Body       *BlockStatement
+}
+
+func (fl *FunctionLiteral) expressionNode()      {}
+func (fl *FunctionLiteral) TokenLiteral() string { return fl.TokenLiteral() }
+func (fl *FunctionLiteral) String() string {
+	var out bytes.Buffer
+	params := []string{}
+	for _, p := range fl.Parameters {
+		params = append(params, p.String())
+	}
+
+	out.WriteString(fl.TokenLiteral())
+	out.WriteString("(")
+	out.WriteString(strings.Join(params, ", "))
+	out.WriteString(") ")
+	out.WriteString(fl.Body.String())
+
+	return out.String()
 }
 
 type PrefixExpression struct {
